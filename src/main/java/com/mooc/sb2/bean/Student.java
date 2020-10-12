af5edf733b20813c00d788e6c0d6c50a50879f9e
hujie <810824629@qq.com>
2020-10-12 20:31:18 +0800

    删除多余代码

--- src/main/java/com/mooc/sb2/bean/Student.java
+++ src/main/java/com/mooc/sb2/bean/Student.java
@@ -8,7 +8,6 @@ import java.io.Serializable;
 
 public class Student extends BaseEntity {
 
-//    private static final long serialVersionUID = -277891939915574508L;
     private String id;
     private String name;
     private String score;
--- src/main/java/com/mooc/sb2/service/StudentService.java
+++ src/main/java/com/mooc/sb2/service/StudentService.java
@@ -6,14 +6,9 @@ import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 
-//@CacheConfig(cacheNames = "student")
 public interface StudentService {
-//    @CachePut(key = "#p0.id")
     int add(Student student);
-//    @CachePut(key = "#p0.id")
     int update(Student student);
-//    @CacheEvict(key = "#p0", allEntries = true)
     int deleteByIds(Student id);
-//    @Cacheable(key = "#p0")
     Student queryStudentById(String id);
 }
