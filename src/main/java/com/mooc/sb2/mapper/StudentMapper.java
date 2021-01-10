70505d09e96ea14732c0a63ce8d33b0265085d32
hujie <810824629@qq.com>
2021-01-10 23:32:47 +0800

    修改配置

--- src/main/java/com/mooc/sb2/mapper/StudentMapper.java
+++ src/main/java/com/mooc/sb2/mapper/StudentMapper.java
@@ -9,18 +9,18 @@ import java.util.Date;
 @Component
 @Mapper
 public interface StudentMapper {
-    @Insert("insert into student(id, name, score, dateTime) values(#{id}, #{name}, #{score}, #{dateTime})")
+//    @Insert("insert into student(id, name, score, dateTime) values(#{id}, #{name}, #{score}, #{dateTime})")
     int add(Student student);
-    @Update("update student set name=#{name}, score=#{score}, dateTime=#{dateTime} where id=#{id}")
+//    @Update("update student set name=#{name}, score=#{score}, dateTime=#{dateTime} where id=#{id}")
     int update(Student student);
-    @Delete("delete from student where id=#{id}")
+//    @Delete("delete from student where id=#{id}")
     int deleteByIds(Student id);
-    @Select("select * from student where id=#{id}")
-    @Results(id = "student",value= {
-            @Result(property = "id", column = "id", javaType = String.class),
-            @Result(property = "name", column = "name", javaType = String.class),
-            @Result(property = "score", column = "score", javaType = String.class),
-            @Result(property = "dateTime", column = "dateTime", javaType = Date.class)
-    })
+//    @Select("select * from student where id=#{id}")
+//    @Results(id = "student",value= {
+//            @Result(property = "id", column = "id", javaType = String.class),
+//            @Result(property = "name", column = "name", javaType = String.class),
+//            @Result(property = "score", column = "score", javaType = String.class),
+//            @Result(property = "dateTime", column = "dateTime", javaType = Date.class)
+//    })
     Student queryStudentById(String id);
 }
